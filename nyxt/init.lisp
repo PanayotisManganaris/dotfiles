a90a44d84b3f8b21b50ee3caa82a0b9d7f255d36
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-02-05 17:28:47 -0500

    make nyxt functions remote executable. init and load custom themes.

--- nyxt/init.lisp
+++ nyxt/init.lisp
@@ -1,1 +1,21 @@
 ;; my nyxt browser config
+;; load dependencies
+(in-package #:nyxt-user)
+
+;; make externally controllable
+(define-configuration browser
+    ((remote-execution-p :t)))
+
+;; my keymap
+(define-configuration buffer
+  ((override-map (let ((map (make-keymap "override-map")))
+                   (define-key map
+                       "M-x" 'execute-command
+;;		       "C-space" 'mark-set
+		       "s-space" 'nothing)))))
+
+;; custom nyxt visual theme
+;; Import Files
+(dolist (file (list (nyxt-init-file "statusline.lisp")
+                    (nyxt-init-file "stylesheet.lisp")))
+  (load file))
