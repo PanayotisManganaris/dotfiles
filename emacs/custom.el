65a930b3dbeb29b994f5aa4411669dcdf90dab18
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-06-07 01:38:57 -0400

    custom file

---
+++ emacs/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet-snippets wucuo which-key wgrep vertico use-package swiper sudo-edit slime rainbow-mode projectile popper poetry perspective org-roam-bibtex org-ref org-pdftools org-download orderless ol-notmuch ob-ipython multiple-cursors marginalia magit lsp-mode jupyter ivy-yasnippet ivy-bibtex gnuplot-mode gnuplot embark-consult elfeed eldoc doom-themes dogears corfu consult-dir conda company citar cape bufler))
+ '(safe-local-variable-values '((conda-project-env-path "/opt/miniconda3/envs/cmcl/"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-yellow ((t (:background "grey30" :foreground "yellow3"))))
+ '(jupyter-repl-traceback ((t (:background "#1e1e1e"))))
+ '(org-ellipsis ((t (:foreground "gray40" :underline nil)))))
